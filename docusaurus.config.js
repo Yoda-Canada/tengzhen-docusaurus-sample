42c97738b9b1a3120be87cfde0d9736f8528eded
Tengzhen <tengzhenzhao@gmail.com>
2021-10-25 22:35:26 -0400

    diploy in github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,31 +8,32 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://Yoda-Canada.github.io',
+  baseUrl: '/tengzhen-docusaurus-sample/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Yoda-Canada', // Usually your GitHub org/user name.
+  projectName: 'tengzhen-docusaurus-sample', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
       '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        // docs: {
-        //   sidebarPath: require.resolve('./sidebars.js'),
-        //   // Please change this to your repo.
-        //   editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        // },
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/Yoda-Canada.github.io/tengzhen-docusaurus-sample/edit/main/website/',
+        },
         blog: {
 
           path: "./blog",
           showReadingTime: true,
-          // Please change this to your repo.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+         // Please change this to your repo.
+          editUrl:
+            'https://github.com/Yoda-Canada.github.io/tengzhen-docusaurus-sample/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -51,12 +52,12 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'intro',
-          //   position: 'left',
-          //   label: 'Tutorial',
-          // },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
           {to: '/blog', label: 'Blog', position: 'right'},
           {
             href: 'https://github.com/facebook/docusaurus',
