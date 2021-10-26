baed329e32db10fcf67798364b74837f6bb41136
Tengzhen <tengzhenzhao@gmail.com>
2021-10-26 11:25:36 -0400

    change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Tengzhen\' Site',
+  title: 'Tengzhen Sample Site',
   tagline: 'OSD 600 Lab-6',
   url: 'https://Yoda-Canada.github.io',
   baseUrl: '/tengzhen-docusaurus-sample/',
